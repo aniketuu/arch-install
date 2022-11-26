2a2a0e45ca97778c1329ed8e11fdc06b53bce6cf
aniketuu <58410742+aniketuu@users.noreply.github.com>
2022-11-26 14:04:51 +0530

    Update base-install.sh

--- base-install.sh
+++ base-install.sh
@@ -27,6 +27,7 @@ echo ""
 read -p "want 32-bit packages? [y/N] " BIT32
 if [[ $BIT32 = "y" ]]; then
   vim /etc/pacman.conf
+  Pacman -Syu --noconfirm
   LIB32_MESA="lib32-mesa"
   LIB32_NVI_UTLS="lib32-nvidia-utils"
 fi
