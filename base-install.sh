78aeb2987c6c8cac4a64731af8c451b55fd7e050
aniketuu <58410742+aniketuu@users.noreply.github.com>
2022-11-26 12:30:54 +0530

    Update base-install.sh

--- base-install.sh
+++ base-install.sh
@@ -23,6 +23,7 @@ fi
 # edit pacman conf
 LIB32_MESA=""
 LIB32_NVI_UTLS=""
+echo ""
 read -p "want 32-bit packages? [y/N] " BIT32
 if [[ $BIT32 = "y" ]]; then
   vim /etc/pacman.conf
@@ -36,19 +37,44 @@ pacman -S --noconfirm xorg-server xorg-xinit xorg-apps xf86-video-intel mesa $LI
 # nvidia
 read -p "using nvidia? [y/N] " NVI
 if [[ $NVI = "y" ]]; then
+  echo ""
   pacman -S --noconfirm nvidia nvidia-utils $LIB32_NVI_UTLS nvidia-prime nvidia-settings
 fi
 
 # install other stuff
+echo ""
 pacman -S --noconfirm noto-fonts noto-fonts-emoji noto-fonts-cjk ttf-iosevka-nerd ttf-hack-nerd ttf-jetbrains-mono ttf-font-awesome \
   ffmpeg mpv feh zathura zathura-pdf-mupdf firefox kitty \
   zip gzip unzip gunzip xdotool bluez bluez-utils git go pipewire pipewire-pulse xdg-user-dirs numlockx brightnessctl sed \
+  bash-completion htop neofetch \
   xfce4 xfce4-goodies lightdm lightdm-gtk-greeter
-  
 
 systemctl enable bluetooth --now
 systemctl enable lightdm
 xdg-user-dirs-update
 
 sed -i "s/^#greeter-hide-users=false$/greeter-hide-users=false/" /etc/lightdm/lightdm.conf
+
+# install yay
+mkdir /tmp/yay
+git clone https://aur.archlinux.org/yay-git.git /tmp/yay/
+cd /tmp/yay
+makepkg
+echo ""
+ls
+read -p "yay: " YAY
+pacman -U --noconfirm $YAY 
+
+# install qemu/kvm
+read -p "install qemu/kvm? [y/N] " VM
+if [[ $VM = "y" ]]; then
+  pacman -S --noconfirm qemu dhclient openbsd-netcat virt-viewer libvirt dnsmasq dmidecode ebtables virt-install virt-manager bridge-utils edk2-ovmf
+  systemctl enable libvirtd --now
+  mkdir ~/.config/libvirt
+  sudo cp -rv /etc/libvirt/libvirt.conf ~/.config/libvirt/
+  read -p "user: " USER
+  chown $USER:wheel ~/.config/libvirt/libvirt.conf
+  virsh net-autostart default
+fi
+
 exit
